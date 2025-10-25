e4ed8cb21f305b08768050951c537d8e0e940e8d
Hannah Hironaka <hannahhironaka@gmail.com>
2025-10-25 17:51:26 -0400

    Open yes chef search for search

--- Yes Chef/Yes Chef/Search/Search.swift
+++ Yes Chef/Yes Chef/Search/Search.swift
@@ -6,10 +6,24 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
 
 struct Search: View {
     var body: some View {
-        Text("Search")
+        WebView(url: URL(string: "https://yes-chef.ai/search")!)
     }
 }
 
